1dddfe73c8222eea221dd7b0c829e39ddb58dfe3
TheBeepMaster <jordikeijzers@gmail.com>
2018-01-02 11:50:59 +0100

    Create help.js

---
+++ bot/commands/help.js
@@ -0,0 +1,9 @@
+exports.run = function(client, message, args) {
+    return message.reply("Not implented yet.");
+};
+
+exports.help = {
+    "permission_level": 1,
+    "name": "Ban",
+    "usage": `<@User> <Reason>`
+};
