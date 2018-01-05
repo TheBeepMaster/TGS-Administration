c56457e5d92554f127bbcef41529630c7fc596d6
TheBeepMaster <jordikeijzers@gmail.com>
2018-01-05 11:49:13 +0100

    Update index.js

--- bot/index.js
+++ bot/index.js
@@ -9,7 +9,7 @@ client.on("message", message => {
     if (!message.guild.available) return;
 
     if (message.content.startsWith(process.env.PREFIX)) {
-        const command = message.content.split(" ")[0].slice(process.env.PREFIX.length);
+        const command = message.content.split(" ")[0].slice(process.env.PREFIX.length).toLowerCase();
         let args = [];
 
         for (let index = 0; index < message.content.split(" ").length; index++) {
