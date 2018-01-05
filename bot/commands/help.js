4b72c1f1dad35308836862c73407c84673a075ce
TheBeepMaster <jordikeijzers@gmail.com>
2018-01-05 13:58:12 +0100

    Update help.js

--- bot/commands/help.js
+++ bot/commands/help.js
@@ -1,9 +1,24 @@
+const fs = require("fs");
+const discordjs = require("discord.js");
+
 exports.run = function(client, message, args) {
-    return message.reply("Not implented yet.");
+    const embed = new discordjs.RichEmbed();
+    embed.setTitle("Command list & usages");
+    embed.setColor(0xCC3E44);
+    embed.setFooter("© The Gaming Squad, 2018");
+
+    const commands = fs.readdirSync(".");
+    for (let index = 0; index < commands.length; index++) {
+        const command = require(`./${commands[index]}.js`);
+
+        embed.addField(command.name, "Usage: **" + process.env.PREFIX + command.name + " " + command.usage);
+    };
+
+    return message.author.send({embed: embed});
 };
 
 exports.help = {
     "permission_level": 1,
-    "name": "Ban",
-    "usage": `<@User> <Reason>`
+    "name": "Help",
+    "usage": ""
 };
