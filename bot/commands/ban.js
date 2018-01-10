11ff26b1c3cd79b773b3478e20f0ef45c87f0b60
TheBeepMaster <jordikeijzers@gmail.com>
2018-01-10 19:59:25 +0100

    Update ban.js

--- bot/commands/ban.js
+++ bot/commands/ban.js
@@ -14,45 +14,41 @@ exports.run = function(client, message, args) {
     const member = message.guild.members.find("id", user.id);
 
     if (member) { // To be on the safe side.
-        if (member.roles.find("name", "Founders") == false) {
-            if (typeof(reason) == "string") {
-                let banRole = message.guild.roles.find("name", "Banned");
-                if (banRole) {
-                    let memberRoles = member.roles.array();
-                    const channel = message.channel
-    
-                    for (index = 0; index < member.roles.array().length; index++) {
-                        const role = member.roles.array()[index];
-    
-                        member.removeRole(role, "This user is banned.");
-                    };
-                    member.addRole(banRole, reason);
-    
-                    channel.send(`**${member.displayName}** has been banned for 2 hours.`);
-                    message.delete();
-                    
-                    client.setTimeout(function() {
-                        for (let index = 0; index < memberRoles.length; index++) {
-                            const role = memberRoles[index];
-                            if (!role.name.startsWith("Warning")) {
-                                member.addRole(role);
-                            };
-                        };
-    
-                        member.removeRole(banRole);
-                        member.send("You have been unbanned in **The Gaming Squad**. You can now chat again. If you're not unbanned, contact a staff member.");
-                        channel.send(`Unbanning user **${member.displayName}**.`);
-                    }, 7200000);
-    
-                    return member.send(`You have been banned in **The Gaming Squad**.\n\nFor reason: **${reason}**\nAdministrator: **${message.author.username}**`);
-                } else {
-                    return message.reply(":x: Unable to find the **Banned** role. The user has not been banned.");
+        if (typeof(reason) == "string") {
+            let banRole = message.guild.roles.find("name", "Banned");
+            if (banRole) {
+                let memberRoles = member.roles.array();
+                const channel = message.channel
+
+                for (index = 0; index < member.roles.array().length; index++) {
+                    const role = member.roles.array()[index];
+
+                    member.removeRole(role, "This user is banned.");
                 };
+                member.addRole(banRole, reason);
+
+                channel.send(`**${member.displayName}** has been banned for 2 hours.`);
+                message.delete();
+
+                client.setTimeout(function() {
+                    for (let index = 0; index < memberRoles.length; index++) {
+                        const role = memberRoles[index];
+                        if (!role.name.startsWith("Warning")) {
+                            member.addRole(role);
+                        };
+                    };
+
+                    member.removeRole(banRole);
+                    member.send("You have been unbanned in **The Gaming Squad**. You can now chat again. If you're not unbanned, contact a staff member.");
+                    channel.send(`Unbanning user **${member.displayName}**.`);
+                }, 7200000);
+
+                return member.send(`You have been banned in **The Gaming Squad**.\n\nFor reason: **${reason}**\nAdministrator: **${message.author.username}**`);
             } else {
-                return message.reply(":x: Please provide a valid reason.");
+                return message.reply(":x: Unable to find the **Banned** role. The user has not been banned.");
             };
         } else {
-            return message.reply(":x: You can't ban a founder.");
+            return message.reply(":x: Please provide a valid reason.");
         };
     };
 };
