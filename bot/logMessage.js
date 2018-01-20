45d0ddc46ba41479df0cd6b366fa4beb155c40b7
TheBeepMaster <jordikeijzers@gmail.com>
2018-01-20 10:31:33 +0100

    Update logMessage.js

--- bot/logMessage.js
+++ bot/logMessage.js
@@ -4,13 +4,12 @@ const webhook = new discordjs.WebhookClient(process.env.WEBHOOK_ID, process.env.
 exports.log = function(message) {
     const member = message.guild.members.find("id", message.author.id);
 
-    const embed = new discordjs.RichEmbed();
-    embed.setColor(0xff0000);
-
     if (member) {
-        embed.addField(member.displayName, message.content);
+        const embed = new discordjs.RichEmbed();
+        embed.setColor(0xff0000);
+        embed.addField(member, message.content);
         embed.setFooter("© The Gaming Squad, 2018");
+        
+        webhook.send({embed: embed});
     };
-
-    webhook.send({embed: embed});
 };
