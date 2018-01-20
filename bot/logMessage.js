1cb5eb07bd9ddae6306e01c0faf534d634a3744f
TheBeepMaster <jordikeijzers@gmail.com>
2018-01-20 10:22:21 +0100

    Add files via upload

---
+++ bot/logMessage.js
@@ -0,0 +1,11 @@
+const discordjs = require("discord.js");
+const webhook = new discordjs.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
+
+exports.log = function(message) {
+    const member = message.guild.members.find("id", message.author.id);
+    const embed = new discordjs.RichEmbed();
+    embed.addField(member.displayName, message.content);
+    embed.setFooter("© The Gaming Squad, 2018")
+
+    webhook.send({embed: embed});
+};
