17fc716d53defd9144770ee94dc95982d0a2e12a
TheBeepMaster <jordikeijzers@gmail.com>
2018-02-10 08:07:40 +0100

    Update logMessage.js

--- bot/logMessage.js
+++ bot/logMessage.js
@@ -2,9 +2,9 @@ const discordjs = require("discord.js");
 const webhook = new discordjs.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
 
 exports.log = function(message, type) {
-    const member = message.guild.members.find("id", message.author.id);
+    const member = message.member;
 
-    if (member) {
+    if (member && !message.author.bot) {
         const embed = new discordjs.RichEmbed();
 
         if (type == "CREATE") {
