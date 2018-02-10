57f3b33c219edf73422d00e72d5aa71c93ab6b8a
TheBeepMaster <jordikeijzers@gmail.com>
2018-02-10 08:02:59 +0100

    Update logMessage.js

--- bot/logMessage.js
+++ bot/logMessage.js
@@ -1,14 +1,20 @@
 const discordjs = require("discord.js");
 const webhook = new discordjs.WebhookClient(process.env.WEBHOOK_ID, process.env.WEBHOOK_TOKEN);
 
-exports.log = function(message) {
+exports.log = function(message, type) {
     const member = message.guild.members.find("id", message.author.id);
-
-    console.log(member);
 
     if (member) {
         const embed = new discordjs.RichEmbed();
-        embed.setColor(0xff0000);
+
+        if (type == "CREATE") {
+            embed.setTitle("Message Created");
+            embed.setColor(0xff0000);
+        } else if (type == "DELETE") {
+            embed.setTitle("Message Deleted");
+            embed.setColor(0xe82727);
+        };
+        
         embed.addField("Author: " + member.displayName, "Message: **"+ message.content + "**");
         embed.setFooter("© The Gaming Squad, 2018");
         embed.setTimestamp();
